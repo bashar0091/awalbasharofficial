0b8a03bd46e8643f2586edb428be5c5ce11e4207
unknown <bashar0091>
2022-11-22 14:43:55 +0600

    portfolio update

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,4 +90,23 @@ $(document).ready(function(){
         'wrapAround': true
     })
 
+    // pagination 
+    var items = $(".portolio-list .list-item");
+    var numItems = items.length;
+    var perPage = 6;
+
+    items.slice(perPage).hide();
+
+    $('#pagination-container').pagination({
+        items: numItems,
+        itemsOnPage: perPage,
+        prevText: "&laquo;",
+        nextText: "&raquo;",
+        onPageClick: function (pageNumber) {
+            var showFrom = perPage * (pageNumber - 1);
+            var showTo = showFrom + perPage;
+            items.hide().slice(showFrom, showTo).show();
+        }
+    });
+
 });
